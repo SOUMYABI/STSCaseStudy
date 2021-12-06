c70640da4d7535072ce6d7ef31f2ba2bfe7cd342
soumyabi <soumyabi@LIN25008026.corp.capgemini.com>
2021-12-06 15:53:37 +0530

    first commit

---
+++ api-gateway/src/main/java/com/SpringCloudcs/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.SpringCloudcs.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableEurekaServer
+
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
